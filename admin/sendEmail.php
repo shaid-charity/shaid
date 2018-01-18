38d0b3b0da939c82171a8ef557082073c01171e8
Matthew Watson <matthew.accounts@gmx.com>
2018-01-18 21:25:10 +0000

    Make everything work nicely togther

--- admin/sendEmail.php
+++ admin/sendEmail.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../includes/settings.php';
 require_once '../includes/config.php';
+require_once 'header.php';
 
 if (isset($_GET['action']) && $_GET['action'] == 'write') {
 	// This will eventually allow you to select emails from this screen too
--- admin/usermgmt.php
+++ admin/usermgmt.php
@@ -1,39 +1,5 @@
 <?php
-session_start();
-require_once('db.php');
-require_once('functions.php');
-$query = $con->prepare("SELECT session_number FROM sessions WHERE session_id=? AND session_ip=? AND session_expiration > NOW() ORDER BY session_number DESC;");
-
-$query->bind_param("ss", session_id(), $_SERVER["REMOTE_ADDR"]);
-$query->execute();
-$query->bind_result($session_number);
-$query->fetch();
-$query->close();
-
-if(empty($session_number)){
-  header("Location: login.php");
-  die();
-    //return;
-} else {
-    //check if user has permission to access user management
-  $query = $con->prepare("SELECT user_perm FROM users, sessions WHERE users.user_id = sessions.user_id AND session_number=?");
-  $query->bind_param("s", $session_number);
-  $query->execute();
-  $query->bind_result($user_perm);
-  $query->fetch();
-  $query->close();
-
-  if(!(strpos($user_perm, "U") !== false)){
-    die("You don't have permission to access this page");
-    header("Location: login.php");
-      //return;
-  }
-  
-  $query = $con->prepare("UPDATE sessions SET session_expiration = DATE_ADD(NOW(), INTERVAL 4 HOUR) WHERE session_number=?");
-  $query->bind_param("s", session_id());
-  $query->execute();
-  $query->close();
-}
+require_once 'header.php';
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
   switch ($_POST["action"]) {
