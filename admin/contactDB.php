a978ba7f9349bc3cb96d7774147e900cc90d0942
Matthew Watson <matthew.accounts@gmx.com>
2018-01-18 22:36:33 +0000

    Update pagination, add styling to contact DB.php

--- admin/contactDB.php
+++ admin/contactDB.php
@@ -8,6 +8,16 @@ require_once 'header.php';
 
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.js" crossorigin="anonymous"></script>
+
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
@@ -48,55 +58,68 @@ if (!isset($_GET['action']) || isset($_GET['term'])) {
 		$friends[] = new Friend($db, $row['id']);
 	}
 ?>
-<form action="contactDB.php" method="get">
-	<input type="text" id="contactSearch" name="term" placeholder="<?php echo $searchText; ?>">
-	<input type="submit" value="Search">
-</form>
-<form action="sendEmail.php?action=write" method="post">
-	<table id="contactDB">
-		<tr>
-			<th>Email</th>
-			<th>Forename</th>
-			<th>Surname</th>
-			<th>Delete Email</th>
-			<th>Select</th> <!-- TODO: Not happy with name of column -->
-		</tr>
+<div class="container">
+	<form class="form-inline" action="contactDB.php" method="get">
+		<input class="form-control" type="text" id="contactSearch" name="term" placeholder="<?php echo $searchText; ?>">
+		<input class="btn btn-primary" type="submit" value="Search">
+	</form>
+	<form action="sendEmail.php?action=write" method="post">
+		<table class="table table-hover table-striped" id="contactDB">
+			<tr>
+				<th>Email</th>
+				<th>Forename</th>
+				<th>Surname</th>
+				<th>Delete Email</th>
+				<th>Select</th> <!-- TODO: Not happy with name of column -->
+			</tr>
 
 <?php
 
 	foreach ($friends as $f) {
-		echo '<tr><td>' . $f->getEmail() . '</td><td>' . $f->getForename() . '</td><td>' . $f->getSurname() . '</td><td><a href="contactDB.php?action=delete&id=' . $f->getID() . '">Delete</a></td><td><input class="checkbox" type="checkbox" name="' . $f->getEmail() . '" value="' . $f->getEmail() . '"></tr>';
+		echo '<tr><td>' . $f->getEmail() . '</td><td>' . $f->getForename() . '</td><td>' . $f->getSurname() . '</td><td><a class="delete" href="contactDB.php?action=delete&id=' . $f->getID() . '">Delete</a></td><td><input class="checkbox" type="checkbox" name="' . $f->getEmail() . '" value="' . $f->getEmail() . '"></tr>';
 	}
 
 ?>
 
-	</table>
-	<input type="submit" value="Send email"> <div id="numChecked">0</div>
-</form>
-<form action="sendEmail.php?action=write" method="post">
-	<input type="hidden" name="all" value="yes">
-	<input type="submit" value="Send email to all contacts">
-</form>
+		</table>
+		<nav>
+			<ul class="pagination justify-content-center">
 <?php
 
-echo $pagination->getFirstAndBackLinks() . $pagination->getCurrentPageLinks() . $pagination->getNextAndLastLinks();
+echo $pagination->getFirstAndBackLinks() . $pagination->getBeforeLinks() . $pagination->getCurrentPageLinks() . $pagination->getAfterLinks() . $pagination->getNextAndLastLinks();
+
+?>
+
+			</ul>
+		</nav>
+		<input class="btn btn-primary" type="submit" value="Send email to selected" name="sendEmail"> <label for="sendEmail" id="numChecked">0</label> <label>currently selected emails</label>
+	</form>
+	<form action="sendEmail.php?action=write" method="post">
+		<input type="hidden" name="all" value="yes">
+		<input class="btn btn-primary" type="submit" value="Send email to all contacts">
+	</form>
+
+<?php
 
 } else if ($_GET['action'] == 'delete') {
 	// Get the Friend we want
 	$friend = new Friend($db, $_GET['id']);
 	$friend->deleteFromDB();
 ?>
-<p>Friend delete successfully. <a href="contactDB.php">Go back to contact DB.</a></p>
-
+	<p>Friend delete successfully. <a href="contactDB.php">Go back to contact DB.</a></p>
+</div>
 <?php
 } 
 ?>
 
 </body>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="js/script.js"></script>
 <script>
+$('.delete').click(function(){
+    return confirm("Are you sure you want to delete the contact?");
+});
+
 $('.checkbox').change(function() {
 	if (this.checked) {
 		$('#numChecked').text(parseInt($('#numChecked').text()) + 1);
