4675137f196dd94ceb5f83b17f28779c45ca020c
Matthew Watson <matthew.accounts@gmx.com>
2018-01-18 21:06:03 +0000

    Nasty hacks to allow both mine and Dmytro's code to work

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@ define( "APP_ROOT", realpath( dirname( __FILE__ ) ).'/../');
 
 require_once 'autoload.php';
 require_once APP_ROOT . 'vendor/autoload.php';// Autoloads the packages installed via Composer that we use
-require_once 'db.php' // Require Dmyro's db.php
+require_once 'db.php'; // Require Dmyro's db.php
 
 // If someone tries to load this file directly, then deny access
 // The IN_APP constant is defined in settings.php
@@ -11,13 +11,15 @@ if (!defined('IN_APP')) {
 	die('You cannot access this file directly');
 }
 
-/*
+
 // May not be needed due to Dmytro's db.php
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
 // Databse constants that will be defined throughout the application
+// TODO: We are opening two connections to the DB so that both my code and Dmytro's will work.
+// This needs fixing ASAP
 define('DB_HOST', 'localhost');
 define('DB_PORT', '8889');
 define('DB_NAME', 'gp');
@@ -34,7 +36,7 @@ try {
 } catch (PDOException $e) {
 	echo 'Error from config.php: <br />';
 	echo $e->getMessage();
-}*/
+}
 
 // Settings for sending emails via SwiftMailer
 define('EMAIL_ADDRESS', 'matthew.accounts@gmx.com');
