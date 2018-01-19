c677d455a9a76984d1367b560bd7c833430861f9
Matthew Watson <matthew.accounts@gmx.com>
2018-01-19 00:40:23 +0000

    Styled send email

--- admin/login.php
+++ admin/login.php
@@ -46,13 +46,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
@@ -60,12 +59,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <div class="panel panel-default" style="border-radius:18px; overflow:hidden; padding:15px">
       <form method="POST">
         <div class="input-group">
-          <div class="input-group-addon"><span class="glyphicon glyphicon-user"></div>
+          <div class="input-group-prepend"><span class="fa fa-user input-group-text pt-2"></div>
             <input class="form-control" type="text" name="user" placeholder="Username"></br>
           </div>
         </br>
         <div class="input-group">
-          <div class="input-group-addon"><span class="glyphicon glyphicon-lock"></div>
+          <div class="input-group-prepend"><span class="fa fa-key input-group-text pt-2"></div>
             <input class="form-control" type="password" name="pass" placeholder="Password"></br>
           </div>
         </br>
--- admin/sendEmail.php
+++ admin/sendEmail.php
@@ -10,13 +10,17 @@ if (isset($_GET['action']) && $_GET['action'] == 'write') {
 		throw new Exception('No contacts were selected!');
 	}
 
-	if (isset($_POST['all'])) {
-		$stmt = $db->query("SELECT `email` FROM `gp_friends`");
-
+	$toText = '';
+	if (isset($_POST['type'])) {
+		if ($_POST['type'] == 'all') {
+			$stmt = $db->query("SELECT `email` FROM `gp_friends`");
+		}
+		
 		$emails = [];
 		foreach ($stmt as $row) {
 			$emails[] = $row['email'];
 		}
+		$toText = 'All contacts';
 
 		$_SESSION['emails'] = $emails;
 	} else {
@@ -24,21 +28,84 @@ if (isset($_GET['action']) && $_GET['action'] == 'write') {
 		$emails = [];
 
 		foreach ($_POST as $email) {
-			$emails[] = $email;
+			if (!empty($email)) {
+				$emails[] = $email;
+				$toText .= $email . '; ';
+			}
 		}
 
 		$_SESSION['emails'] = $emails;
 	}
 
 ?>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<form action="sendEmail.php?action=send" method="post">
-	<label for="subject">Subject:</label> <input type="text" name="subject"><br />
-	<label for="to">To:</label> <?php foreach ($emails as $email) { echo $email . '; '; } ?><br />
-	<label for="message">Message:</label><br />
-	<textarea name="message"></textarea>
-	<input type="submit" value="Send email">
-</form>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<!-- Top navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light mb-2">
+	  <a class="navbar-brand" href="#">SHAID Admin Panel</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarNav">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="contactDB.php">Contact DB <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="usermgmt.php">User Management</a>
+	      </li>
+	    </ul>
+
+	    <form class="form-inline my-2 my-lg-0" method="POST" style="float:right;">
+        	<input type="hidden" name="action" value="LOGOUT"/>
+        	<input type='submit' class='btn btn-outline-danger my-2 my-sm-0' value="Log Out" />
+      	</form>
+	  </div>
+	</nav>
+
+	<div class="container">
+		<div class="page-header">
+			<h1>Send Email</h1>
+		</div>
+		<br />
+		<form action="sendEmail.php?action=send" method="post">
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label" for="subject">Subject:</label> 
+				<div class="col-sm-10">
+					<input class="form-control" placeholder="Subject" type="text" name="subject">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label" for="to">To:</label> 
+				<div class="col-sm-10">
+					<input type="text" readonly class="form-control-plaintext" value="<?php echo $toText; ?>">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label" for="message">Message:</label>
+				<div class="col-sm-10">
+					<textarea class="form-control" rows="15" name="message"></textarea>
+				</div>
+			</div>
+			<div class="form-group row">
+				<label class="col-sm-2 col-form-label"></label>
+				<input class="btn btn-primary ml-3" type="submit" value="Send email">
+			</div>
+		</form>
+
+	</div>
 
 <?php
 
