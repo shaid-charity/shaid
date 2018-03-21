161f5d0f6392771015aa63c2a198302c6551b041
Caleb Hamilton <cjlhamilton@gmail.com>
2018-03-21 19:20:11 +0000

    Made template for blog dev

---
+++ front-end/blog.php
@@ -0,0 +1,25 @@
+<?php
+    $root=pathinfo($_SERVER['SCRIPT_FILENAME']);
+    define('BASE_FOLDER',  basename($root['dirname']));
+    define('SITE_ROOT',    realpath(dirname(__FILE__)));
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SHAID</title>
+	<?php
+		require_once(SITE_ROOT . '/includes/global_head.php');
+	?>
+</head>
+<body>
+	<?php
+		require_once(SITE_ROOT . '/includes/header.php');
+	?>
+	<main id="main-content">
+	</main>
+	<?php
+		require_once(SITE_ROOT . '/includes/footer.php');
+		require_once(SITE_ROOT . '/includes/global_scripts.php');
+	?>
+</body>
+</html>
