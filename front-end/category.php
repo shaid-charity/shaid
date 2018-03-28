c85a9c743cbdad91359e7b7a0281e593f8f982e6
Matthew Watson <matthew.accounts@gmx.com>
2018-03-28 18:59:03 +0100

    Finished category listing

--- front-end/category.php
+++ front-end/category.php
@@ -2,6 +2,9 @@
     $root=pathinfo($_SERVER['SCRIPT_FILENAME']);
     define('BASE_FOLDER',  basename($root['dirname']));
     define('SITE_ROOT',    realpath(dirname(__FILE__)));
+
+    require_once '../back-end/includes/settings.php';
+	require_once '../back-end/includes/config.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,27 +29,80 @@
 					<div class="page-title">
 						<h1>Category Name</h1>
 					</div>
+					<?php
+						// Check an ID was given and that it exists
+						if (!isset($_GET['id'])) {
+					?>
+					<div class="article-message-banner">
+						No category ID given.
+					</div>
+
+					<?php
+						} else {
+							// Check the ID exists
+							try {
+								$category = new Category($db, $_GET['id']);
+							} catch (Exception $e) {
+					?>
+					<div class="article-message-banner">
+						Check the selected category exists!
+					</div>
+
+					<?php
+							}
+						}
+					?>
 					<section id="articles-list">
+
+						<?php
+							// Get all posts in category
+							// Set up the pagination
+							$pagination = new Pagination($db, "SELECT id FROM `posts` WHERE `category_id` = ?", array($_GET['id']));
+							$pagination->totalRecords();
+							$pagination->setLimitPerPage(5);
+							$currentPage = $pagination->getPage();
+
+							// Select the correct number of records from the DB
+							if (isset($_GET['page'])) {
+								$startFrom = ($_GET['page'] - 1) * 5;
+							} else {
+								$startFrom = 0;
+							}
+
+							// Get all posts, order by descending date
+							$stmt = $db->prepare("SELECT `id` FROM `posts` WHERE `category_id` = ? ORDER BY `datetime-last-modified` DESC LIMIT $startFrom, 5");
+							$stmt->execute([$_GET['id']]);
+								
+							foreach ($stmt as $row) {
+								$p = new Post($db, $row['id']);
+
+								// Decide which image we will show (do this here so there is less inline PHP below)
+								if ($p->getImagePath() == null) {
+									$imageCSS = 'background-image: url(\'assets/img/placeholder/blog_image.jpg\');';
+								} else {
+									$imageCSS = 'background-image: url(\'../back-end/admin/' . htmlentities($p->getImagePath()) . '\');';
+								}
+						?>
+						
 						<div class="articles-list-entry">
-							<a class="articles-list-entry-thumb" href="viewpost.php"></a>
+							<a class="articles-list-entry-thumb" href="viewpost.php" style="<?php echo $imageCSS; ?>"></a>
 							<div class="articles-list-entry-info">
-								<a href="viewpost.php"><h2>A Most Recent Blog Post About Homelessness</h2></a>
+								<a href="viewpost.php"><h2><?php echo $p->getTitle(); ?></h2></a>
 								<p>A description of the most recent blog post.</p>
 								<div class="articles-list-entry-actions">
-									<a href="category.php">Category Name</a>
+									<ul>
+										<li>
+											<span><i class="zmdi zmdi-calendar"></i> <time datetime="<?php echo $p->getDatePublished(); ?>"><?php echo $p->getDatePublished(); ?></time></span>
+										</li>
+										<li>
+											<a href="category.php"><?php echo $p->getCategory()->getName(); ?></a>
+										</li>
+									</ul>
 								</div>
 							</div>
 						</div>
-						<div class="articles-list-entry">
-							<a class="articles-list-entry-thumb" href="viewpost.php"></a>
-							<div class="articles-list-entry-info">
-								<a href="viewpost.php"><h2>Another Blog Post About Homelessness</h2></a>
-								<p>A shorter description about this post.</p>
-								<div class="articles-list-entry-actions">
-									<a href="category.php">Category Name</a>
-								</div>
-							</div>
-						</div>
+
+						<?php } ?>
 					</section>
 				</section>
 				<aside id="sidebar">
