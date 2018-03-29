23a6a7ca3cbcd40e901374abfb70dcd45269cabd
Matthew Watson <matthew.accounts@gmx.com>
2018-03-29 16:03:21 +0100

    Refactored the blog index to use the correct Post class methods

--- front-end/blog/index.php
+++ front-end/blog/index.php
@@ -57,9 +57,9 @@
 						?>
 
 						<div class="articles-list-entry">
-							<a class="articles-list-entry-thumb" href="<?php echo $post->getCategory()->getName() . '/' . $post->getID() . '-' . $post->getTitle(); ?>/" style="<?php echo $imageCSS; ?>"></a>
+							<a class="articles-list-entry-thumb" href="<?php echo $post->getLink(); ?>/" style="<?php echo $imageCSS; ?>"></a>
 							<div class="articles-list-entry-info">
-								<a href="<?php echo $post->getCategory()->getName() . '/' . $post->getID() . '-' . $post->getTitle(); ?>/"><h2><?php echo $post->getTitle(); ?></h2></a>
+								<a href="<?php echo $post->getLink(); ?>/"><h2><?php echo $post->getTitle(); ?></h2></a>
 								<p>A description of the most recent blog post.</p>
 								<div class="articles-list-entry-actions">
 									<ul>
