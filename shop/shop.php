889f37390d85b85d0142e043a08cae628fe354e2
Caleb Hamilton <cjlhamilton@gmail.com>
2018-04-15 20:47:29 +0100

    Updated view and added images

--- shop/shop.php
+++ shop/shop.php
@@ -8,97 +8,111 @@
 <body>
 	<?php
 
-		$MODE = 'production';
+		/* -------------------- SETUP -------------------- */
 
-		/*
-			REMOVE THESE LINES
-		*/
-		ini_set('display_errors', 1);
-		ini_set('display_startup_errors', 1);
-		error_reporting(E_ALL);
-		/*
-			REMOVE THESE LINES
-		*/
-
+		// Load ebay-sdk
 		require './includes/lib/ebay-sdk/ebay-sdk-php-autoloader.php';
 
+		// Load config
 		$config = require './config.php';
-/**
- * The namespaces provided by the SDK.
- */
-use \DTS\eBaySDK\Constants;
-use \DTS\eBaySDK\Trading\Services;
-use \DTS\eBaySDK\Trading\Types;
-use \DTS\eBaySDK\Trading\Enums;
-/**
- * Create the service object.
- */
-$service = new Services\TradingService([
-    'credentials' => $config[$MODE]['credentials'],
-    'siteId'      => Constants\SiteIds::GB
-]);
+
+		// Set mode to use with ebay-sdk
+		$MODE = 'production';
+
+		// Set user to view items of
+		$USER = 'dhammatek';
+
+		// The namespaces provided by the SDK.
+		use \DTS\eBaySDK\Constants;
+		use \DTS\eBaySDK\Trading\Services;
+		use \DTS\eBaySDK\Trading\Types;
+		use \DTS\eBaySDK\Trading\Enums;
+		
+		// Create the service object.
+		$service = new Services\TradingService([
+		    'credentials' => $config[$MODE]['credentials'],
+		    'siteId'      => Constants\SiteIds::GB
+		]);
 
 
-/* -------------------- BUILD REQUEST -------------------- */
+		/* -------------------- BUILD REQUEST -------------------- */
 
-/**
- * Create the request object.
- */
-$request = new Types\GetSellerListRequestType();
+		// Create the request object.
+		$request = new Types\GetSellerListRequestType();
 
-/**
- * An user token is required when using the Trading service.
- */
-$request->RequesterCredentials = new Types\CustomSecurityHeaderType();
-$request->RequesterCredentials->eBayAuthToken = $config[$MODE]['userAuthToken'];
+		// An user token is required when using the Trading service.
+		$request->RequesterCredentials = new Types\CustomSecurityHeaderType();
+		$request->RequesterCredentials->eBayAuthToken = $config[$MODE]['userAuthToken'];
 
-$request->Pagination = new Types\PaginationType();
-$request->Pagination->EntriesPerPage = 10;
-//$request->Sort = Enums\ItemSortTypeCodeType::C_CURRENT_PRICE_DESCENDING;
+		// Set pagination info
+		$request->Pagination = new Types\PaginationType();
+		$request->Pagination->EntriesPerPage = 10;
+		// $request->Sort = Enums\ItemSortTypeCodeType::C_CURRENT_PRICE_DESCENDING;
 
-$request->StartTimeFrom = DateTime::createFromFormat('d/m/Y', '23/02/2018');
-$request->StartTimeTo   = DateTime::createFromFormat('d/m/Y', '15/04/2018');
+		$request->StartTimeFrom = DateTime::createFromFormat('d/m/Y', '23/02/2018');
+		$request->StartTimeTo   = DateTime::createFromFormat('d/m/Y', '15/04/2018');
 
-$request->DetailLevel[] = Enums\DetailLevelCodeType::C_ITEM_RETURN_DESCRIPTION;
+		$request->DetailLevel[] = Enums\DetailLevelCodeType::C_ITEM_RETURN_DESCRIPTION;
 
-$request->UserID = 'dhammatek';
+		$request->UserID = $USER;
 
-$pageNum = 1;
-$request->Pagination->PageNumber = $pageNum;
+		// Page
+		$pageNum = 1;
+		if (isset($_GET["page"])) {
+			$pageNum = intval(htmlspecialchars($_GET["page"]));
+
+			// TODO: Validate page number
+			// ...
+		} else {
+			echo "<p>[no page number set; displaying page 1]</p>";
+		}
+		$request->Pagination->PageNumber = $pageNum;
 
 
-/* -------------------- SEND REQUEST -------------------- */
+		/* -------------------- PARSE RESPONSE -------------------- */
 
-$response = $service->getSellerList($request);
-/**
- * Output the result of calling the service operation.
- */
-if (isset($response->Errors)) {
-    foreach ($response->Errors as $error) {
-        printf(
-            "%s: %s\n%s\n\n",
-            $error->SeverityCode === Enums\SeverityCodeType::C_ERROR ? 'Error' : 'Warning',
-            $error->ShortMessage,
-            $error->LongMessage
-        );
-    }
-    die();
-}
-$totalPages = $response->PaginationResult->TotalNumberOfPages;
-echo "<h1>Results for page $pageNum of $totalPages</h1>";
-if ($response->Ack !== 'Failure') {
-	echo "<ul>";
-    foreach ($response->ItemArray->Item as $item) {
-        printf(
-            "<li>(%s) %s <a href=\"%s\">View item</a></li>",
-            $item->ItemID,
-            $item->Title,
-            $item->ListingDetails->ViewItemURL
-        );
-    }
-	echo "</ul>";
-}
-?>
-	</ul>
+		$response = $service->getSellerList($request);
+		/**
+		 * Output the result of calling the service operation.
+		 */
+		if (isset($response->Errors)) {
+		    foreach ($response->Errors as $error) {
+		        printf(
+		            "%s: %s\n%s\n\n",
+		            $error->SeverityCode === Enums\SeverityCodeType::C_ERROR ? 'Error' : 'Warning',
+		            $error->ShortMessage,
+		            $error->LongMessage
+		        );
+		    }
+		    die();
+		}
+		$totalPages = $response->PaginationResult->TotalNumberOfPages;
+		echo "<h1>Results for page $pageNum of $totalPages</h1>";
+		if ($response->Ack !== 'Failure') {
+
+			// Display list of results from requested page
+			echo "<ul>";
+		    foreach ($response->ItemArray->Item as $item) {
+		        printf(
+		            "<li>(%s) %s <a href=\"%s\">View item</a>
+		            <br>
+		            	<img src=\"%s\" width=\"200\">
+		            <br>
+		            </li>",
+		            $item->ItemID,
+		            $item->Title,
+		            $item->ListingDetails->ViewItemURL,
+		            $item->PictureDetails->PictureURL[0]
+		        );
+		    }
+			echo "</ul>";
+
+			// Pagination to navigate pages
+			echo "<ul>";
+			echo "<li><a href=\"?page=" . ($pageNum - 1) . "\">Previous page</a></li>";
+			echo "<li><a href=\"?page=" . ($pageNum + 1) . "\">Next page</a></li>";
+			echo "</ul>";
+		}
+	?>
 </body>
 </html>
