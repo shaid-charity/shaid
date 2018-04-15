797a2d5df13f8f4b9ac84b7eb8628d9cd5c87602
Caleb Hamilton <cjlhamilton@gmail.com>
2018-04-15 20:16:20 +0100

    Working example

--- shop/config.php
+++ shop/config.php
@@ -14,18 +14,14 @@ return [
             'appId' => 'CalebHam-SHAIDSto-SBX-27879bc6b-3f7680ae',
             'certId' => 'abd155e5-8fce-43f2-b303-fb718882add8',
         ],
-        'authToken' => 'YOUR_SANDBOX_USER_TOKEN_APPLICATION_KEY',
-        'oauthUserToken' => 'YOUR_SANDBOX_OAUTH_USER_TOKEN',
-        'ruName' => 'YOUR_SANDBOX_RUNAME'
+        'userAuthToken' => 'AgAAAA**AQAAAA**aAAAAA**aJDTWg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6wFk4agCJGLogmdj6x9nY+seQ**XI0EAA**AAMAAA**LU32CWD9BxIn0azLHbWVg/cXYY1ENAptP/D/x/JBCuqjVETvAi1RTc2mWH1jacAqbrF+xjI+7efpNvbr7ufQ657GJt/s8aWzGAR0JQn2VRkR+vUnEkAuR81AP+yd8CHOggUPp4ZNfuFre28gSYXeOcNuU/D7NPqAHtwMYD07QvocG/jDjoZYCUuLJR0Q0/bwAixVcth1kuZ/2FNJfL9XATK1jnudzht4ccQzZtDLN3TzQMOlxqdGaYf+ewIT/v5+2+WDFAiPM6pqYWdjMTccEL5Tazd1vriJtnmTnMDoYT3uhsm0kdG2HkN8mWeOW4WWaA8761a4SCboAgJs+hQP6wbFNr+jYXoIEpMzQ7eBtbHuUoBU+T01MtDpWWKj5VKHyDCCHmEDUQyM4fZsjo13Fxc2JCB3GktbfpHlVp8Qrq8T6UOk/dGXi/yWjtCU8W/W5p86sMt2Pk3uhvYcird9OxcXsOrcXWeq2inbavvhP2iDzmVS3EImHFKsXJpStXaWV1T+rSLVA79GDUBhaz9gaCKfd1NsG0pmnTRIsxAwIqVoNzO+5chjK6+dxVH3z1jnG4rWgRtNrqMD0Z+woL7AAthlxeYfCee3Z17vTfLukwLXqRrjsszUBsKlNLIoF3GkA9aA62C82jt5rsJ1FxJlRyKeEP0DZ3sSJZenN3U22Q6m5useJyXPZPSI6vdYebqUZMN29WGsyif3RYKI3y5eTdIgnF4pJllNkXVCx6WDd82W9/0VBqy7/OixKHnTdFzZ'
     ],
     'production' => [
         'credentials' => [
-            'devId' => 'PRD-787d01078b28-4d38-4bef-84ab-c4d4',
+            'devId' => 'abd155e5-8fce-43f2-b303-fb718882add8',
             'appId' => 'CalebHam-SHAIDSto-PRD-2787d0107-02f8ef26',
-            'certId' => 'abd155e5-8fce-43f2-b303-fb718882add8',
+            'certId' => 'PRD-787d01078b28-4d38-4bef-84ab-c4d4',
         ],
-        'authToken' => 'YOUR_PRODUCTION_USER_TOKEN_APPLICATION_KEY',
-        'oauthUserToken' => 'YOUR_PRODUCTION_OAUTH_USER_TOKEN',
-        'ruName' => 'YOUR_PRODUCTION_RUNAME'
+        'userAuthToken' => 'AgAAAA**AQAAAA**aAAAAA**uJDTWg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6AEl4qjCZSCoQidj6x9nY+seQ**G0AEAA**AAMAAA**SPA3qYpmdV2gniJLzAoFqqb6Y/oKZzNCG321TdmJUXk03cx8LPwKX5GrF2C6fsIbmcUsCEJQ4xMYYRRsT3NNx1ysui7klicUmOSB08+pbzwNtQ5M7hLIAJ1Op7F382KTnnWxNbOHsbnRl9jn8jhgOntEabvmWdJOTJ2fZakXho8E24KVoqKJnxRP+am33R0O40vvSeJhfiUA5GuRZAvKHZ3XnTe2CfwPDVXoh5bEBMXEd+sMDL1/727GpltrUnplFXyhoejZ733NgD3SyxVDXx1Wn8+FXWZlJDi/cTvt/KF9pujzrqwn1NGRLiIwETFfrmx8xUmgkUWawUd30VJjHnTQvHvgqDQ9VECQFQdnZv/3X3fXyYmlZXRc+VKEKd6DRnjPLt185iBlVtXJOtoSrMTTpiGOsVgsRNfmEdxVo4c1ynw5JHIALYb6d2TkFSF+Se+9dESYh1hPWyzPwYty0z2rk9I67LBn55ahBkBUQWnsihGhxnBtwqO0Abda1KcSamcF7E0PclwDsGyyKqrXjg83Veg7uKK9Z719JHDq8GbB4cQwoYxt6xc2cO89LQzpllwiUUXm6YJxkjc7BqHtQrUrFU3b+zp/E9P9OVwIUsTughg2DJq4SuCkeGOwxyhvHiPjAp4MhwQUW2WMAXhdwAceG0nmdCy8g/f/eRKBb0YTGuCemaErx0F3OTOYEZjXBACvyRAgxYl+ohWB2vgzCXxu0FfpA2MMlHPRViRn8YM9/qybN6UrTpNvuHHYHw6o'
     ]
 ];
--- shop/shop.php
+++ shop/shop.php
@@ -2,9 +2,16 @@
 <html>
 <head>
 	<title>Shop</title>
+	<style>
+	li {
+		padding-bottom: 100px;
+	}
+	</style>
 </head>
 <body>
 	<?php
+
+		$MODE = 'production';
 
 		/*
 			REMOVE THESE LINES
@@ -19,61 +26,87 @@
 		require './includes/lib/ebay-sdk/ebay-sdk-php-autoloader.php';
 
 		$config = require './config.php';
+/**
+ * The namespaces provided by the SDK.
+ */
+use \DTS\eBaySDK\Constants;
+use \DTS\eBaySDK\Trading\Services;
+use \DTS\eBaySDK\Trading\Types;
+use \DTS\eBaySDK\Trading\Enums;
+/**
+ * Create the service object.
+ */
+$service = new Services\TradingService([
+    'credentials' => $config[$MODE]['credentials'],
+    'siteId'      => Constants\SiteIds::GB
+]);
 
-		/**
-		 * The namespaces provided by the SDK.
-		 */
-		use \DTS\eBaySDK\Constants;
-		use \DTS\eBaySDK\Browse\Services;
-		use \DTS\eBaySDK\Browse\Types;
-		use \DTS\eBaySDK\Browse\Enums;
-		/**
-		 * Create the service object.
-		 */
-		$service = new Services\BrowseService([
-		    'authorization' => $config['production']['oauthUserToken'],
-		    'marketplaceId' => Constants\GlobalIds::GB
-		]);
-		/**
-		 * Create the request object.
-		 */
-		$request = new Types\SearchForItemsRestRequest();
-		/**
-		 * Note how URI parameters are just properties on the request object.
-		 */
-		$request->q = 'Harry Potter';
-		$request->sort = '-price';
-		$request->limit = '10';
-		$request->offset = '0';
-		/**
-		 * Send the request.
-		 */
-		$response = $service->searchForItems($request);
-		/**
-		 * Output the result of calling the service operation.
-		 */
-		printf("\nStatus Code: %s\n\n", $response->getStatusCode());
-		if (isset($response->errors)) {
-		    foreach ($response->errors as $error) {
-		        printf(
-		            "%s: %s\n%s\n\n",
-		            $error->errorId,
-		            $error->message,
-		            $error->longMessage
-		        );
-		    }
-		}
-		if ($response->getStatusCode() === 200) {
-		    foreach ($response->itemSummaries as $item) {
-		        printf(
-		            "(%s) %s: %s %.2f\n",
-		            $item->itemId,
-		            $item->title,
-		            $item->price->currency,
-		            $item->price->value
-		        );
-		    }
-		}
-	?>
+/**
+ * Create the request object.
+ */
+$request = new Types\GetSellerListRequestType();
+/**
+ * An user token is required when using the Trading service.
+ */
+$request->RequesterCredentials = new Types\CustomSecurityHeaderType();
+$request->RequesterCredentials->eBayAuthToken = $config[$MODE]['userAuthToken'];
+
+//$request->Pagination = new Types\PaginationType();
+//$request->Pagination->EntriesPerPage = 10;
+//$request->Pagination->PageNumber = 1;
+
+$request->Pagination = new Types\PaginationType();
+$request->Pagination->EntriesPerPage = 10;
+//$request->Sort = Enums\ItemSortTypeCodeType::C_CURRENT_PRICE_DESCENDING;
+
+$request->StartTimeFrom = DateTime::createFromFormat('d/m/Y', '23/02/2018');
+$request->StartTimeTo   = DateTime::createFromFormat('d/m/Y', '15/04/2018');
+
+$request->DetailLevel[] = Enums\DetailLevelCodeType::C_ITEM_RETURN_DESCRIPTION;
+
+$request->UserID = 'dhammatek';
+
+echo "<ul>";
+
+$pageNum = 1;
+
+do {
+    $request->Pagination->PageNumber = $pageNum;
+
+	/**
+	 * Send the request.
+	 */
+	$response = $service->getSellerList($request);
+	/**
+	 * Output the result of calling the service operation.
+	 */
+	echo "<li>";
+	echo "<p>=== Results for page $pageNum ===</p>";
+	if (isset($response->Errors)) {
+	    foreach ($response->Errors as $error) {
+	        printf(
+	            "%s: %s\n%s\n\n",
+	            $error->SeverityCode === Enums\SeverityCodeType::C_ERROR ? 'Error' : 'Warning',
+	            $error->ShortMessage,
+	            $error->LongMessage
+	        );
+	    }
+	}
+	if ($response->Ack !== 'Failure') {
+        foreach ($response->ItemArray->Item as $item) {
+            printf(
+                "<p>(%s) %s <a href=\"%s\">View item</a></p>",
+                $item->ItemID,
+                $item->Title,
+                $item->ListingDetails->ViewItemURL
+            );
+        }
+    }
+    echo "</li>";
+    $pageNum += 1;
+} while (false === true && $pageNum <= $response->PaginationResult->TotalNumberOfPages);
+echo "</ul>";
+?>
+	</ul>
 </body>
 </html>
