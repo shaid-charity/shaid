b0c785c4991ec997447fc2c5fe4840d3f6d83049
Caleb Hamilton <cjlhamilton@gmail.com>
2018-04-23 14:13:51 +0100

    Changed profile styling

--- blog/viewprofile.php
+++ blog/viewprofile.php
@@ -69,7 +69,7 @@
 					?>
 					<div class="articles-list-container">
 						<section class="profile-header">
-								<img src="<?php echo $postsUser->getAvatarPath(); ?>" alt="<?php echo $postsUser->getFullName(); ?>" class="profile-photo">
+								<img src="<?php echo $postsUser->getAvatarPath(); ?>" alt="<?php echo $postsUser->getFullName(); ?>" class="profile-header-photo">
 								<h2><?php echo $postsUser->getFullName(); ?></h2>
 								<h3><?php echo $postsUser->getCompany()->getName(); ?></h3>
 						</section>
