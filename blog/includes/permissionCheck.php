d1dadc3b228ebb1a6ed5d07efa1853ed8cf99ef2
dmytro_drobisher <dimon161295@gmail.com>
2018-04-23 22:29:18 +0100

    permissions

--- blog/includes/permissionCheck.php
+++ blog/includes/permissionCheck.php
@@ -1,28 +1,23 @@
 <?php
 //permissions for every page that needs them
 $permissionArray = array(
-  "newPost" <= 5,
-  "editPost" <= 5,
-  "newCampaign" <= 3,
-  "editCampaign" <= 3,
-  "newCategory" <= 2,
-  "editCategory" <= 2,
-  "newEvent" <= 2,
-  "editEvent" <= 2,
+  "newPost" => 5,
+  "editPost" => 5,
+  "newCampaign" => 3,
+  "editCampaign" => 3,
+  "newCategory" => 2,
+  "editCategory" => 2,
+  "newEvent" => 2,
+  "editEvent" => 2,
 );
 
 //decide whether or not the user is allowed to access $pageName
 function grantAccess($roleID, $pageName){
-  echo $roleID;
-  echo $pageName;
-  echo $permissionArray[$pageName];
   if($roleID > $permissionArray[$pageName]){
     //access denied
-    echo "false";
     return false;
   } else {
     //access granted
-    echo "true";
     return true;
   }
 }
