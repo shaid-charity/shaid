231210fb113565af8b2c00935b7e20dbe1e7980d
Matthew Watson <matthew.accounts@gmx.com>
2018-04-23 12:49:31 +0100

    Added viewprofile.php

---
+++ blog/includes/blog_modules/user_not_selected.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+        Post does not exist (blog module) - Notifies user the post does not exist.
+        * Only CMS pages should show this.
+    */
+?>
+					<div class="article-message-banner">
+						A user has not been selected!
+					</div>
