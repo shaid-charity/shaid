fe83dbd78eb93406cf9feb29f1ad02ac03865a7c
dmytro_drobisher <dimon161295@gmail.com>
2018-04-24 00:00:37 +0100

    new campaign permission check

--- blog/includes/sidebar_modules/campaigns_list_admin_options.php
+++ blog/includes/sidebar_modules/campaigns_list_admin_options.php
@@ -3,8 +3,9 @@
         View post admin options (sidebar module) - shows admin options for a viewed post.
         * Only CMS pages should show this.
     */
+	require_once __DIR__.'/../permissionCheck.php';
 
-    if ($user != null) {
+    if ($user != null && grantAccess($user->getRoleID(), "newCampaign")) {
 ?>
 					<section>
 						<h1>Admin options</h1>
