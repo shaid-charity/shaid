0a5a2f48e7d55c99a92e3985d60c6c656c616710
Matthew Watson <matthew.accounts@gmx.com>
2018-04-23 13:33:13 +0100

    Add approve button to posts

--- blog/includes/sidebar_modules/post_admin_options.php
+++ blog/includes/sidebar_modules/post_admin_options.php
@@ -11,6 +11,8 @@
 						<div class="sidebar-actions">
 							<a href="/<?php echo INSTALLED_DIR; ?>/editpost.php?id=<?php echo $post->getID(); ?>" type="button" class="button-dark">Edit</a>
 							<a href="/<?php echo INSTALLED_DIR; ?>/editpost.php?action=makeDraft&id=<?php echo $post->getID(); ?>" type="button" class="button-dark">Make draft</a>
+							<!-- Dmytro asked for this -->
+							<a href="/<?php echo INSTALLED_DIR; ?>/afile.php" type="button" class="button-dark">Approve</a>
 						</div>
 					</section>
 
