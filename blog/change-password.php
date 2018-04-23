ca507f712a63329124636479da7e482e9b72cbb8
Matthew Watson <matthew.accounts@gmx.com>
2018-04-23 20:17:24 +0100

    Checked passwords are equal everywhere needed, both client and server side

--- blog/change-password.php
+++ blog/change-password.php
@@ -15,6 +15,15 @@
 		require_once(SITE_ROOT . '/includes/admin/admin_head.php');
 	?>
 	<link href="style/blog.css" rel="stylesheet">
+	<style>
+		.is-valid {
+			border: 2px solid green !important;
+		}
+
+		.is-invalid {
+			border: 2px solid red !important;
+		}
+	</style>
 </head>
 <body>
 	<?php
@@ -38,10 +47,14 @@
 								$result = $stmt->fetch();
 								$resetUser = new User($db, $result['user_id']);
 
-								// (Try to) reset the password
-								$test = $resetUser->resetPassword($_POST['token'], $_POST['pass']);
-								if ($test) {
-									require_once(SITE_ROOT . '/includes/admin/admin_pass_reset_success.php');
+								if ($_POST['pass'] == $_POST['passRetype']) {
+									// (Try to) reset the password
+									$test = $resetUser->resetPassword($_POST['token'], $_POST['pass']);
+									if ($test) {
+										require_once(SITE_ROOT . '/includes/admin/admin_pass_reset_success.php');
+									}
+								} else {
+									require_once(SITE_ROOT . '/includes/admin/admin_pass_reset_failed.php');
 								}
 							} else {
 								require_once(SITE_ROOT . '/includes/admin/admin_pass_reset_invalid_token.php');
@@ -61,16 +74,16 @@
 						<div class="content-grid-title">
 							<h1>Change password</h1>
 							<p>Enter your new password below.</p>
-							<form action="change-password.php" method="post" class="login-form">
+							<form action="change-password.php" method="post" class="login-form" id="passResetForm">
 								<div class="post-input">
 									<label for="pass" class="section-label">Password</label>
 									<input type="password" id="pass" name="pass">
-									<label for="passRepeat" class="section-label">Retype Password</label>
-									<input type="password" id="passRepeat" name="passRepeat">
+									<label for="passRetype" class="section-label">Retype Password</label>
+									<input type="password" id="passRetype" name="passRetype">
 									<input type="hidden" name="token" value="<?php echo $_GET['token'] ?>">
 
 								</div>
-								<button type="submit" class="button-green">Submit</button>
+								<button type="submit" class="button-green" id="submit">Submit</button>
 							</form>
 						</div>
 					</section>
@@ -83,5 +96,50 @@
 		require_once(SITE_ROOT . '/includes/footer.php');
 		require_once(SITE_ROOT . '/includes/global_scripts.php');
 	?>
+	<script>
+		$(document).ready(function(){
+	      var password = "";
+	      
+	      $("#submit").click(function(e) {
+	        if($("#pass").val() != $("#passRetype").val() || !validatePassword(password)) {
+	          e.preventDefault();
+	          $("#passRetype").addClass("has-error");
+	        } else {
+	          $("#passResetForm").submit();
+	        }
+	      });
+
+	      $("#pass").on("keyup", function(){
+	        $("pass").removeClass("is-valid");
+	        $("#pass").removeClass("is-invalid");
+	        password = $("#pass").val();
+
+	        if(validatePassword(password)){
+	          $("#pass").addClass("is-valid");    
+	        } else {
+	          $("#pass").addClass("is-invalid");
+	        }
+	      });
+
+	      $("#passRetype").on("keyup", function(){
+	        $("#passRetype").removeClass("is-valid");
+	        $("#passRetype").removeClass("is-invalid");
+	        
+	        if((password == $("#passRetype").val()) && validatePassword(password)){
+	          $("#passRetype").addClass("is-valid");
+	        } else {
+	          $("#passRetype").addClass("is-invalid");
+	        }
+	      });
+
+	      function validatePassword(pass){
+	        if((password.length >= 8) && (password != password.toLowerCase()) && (/([0-9])+/.test(password))){
+	          return true;          
+	        } else {
+	          return false;
+	        }
+	      }
+	    });
+	</script>
 </body>
 </html>
