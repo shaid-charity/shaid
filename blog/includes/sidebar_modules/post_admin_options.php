f093f334b549be0311209117518d7d1402c2ba3a
dmytro_drobisher <dimon161295@gmail.com>
2018-04-23 14:50:08 +0100

    approve button screw up

--- blog/includes/sidebar_modules/post_admin_options.php
+++ blog/includes/sidebar_modules/post_admin_options.php
@@ -13,7 +13,7 @@
 							<a href="/<?php echo INSTALLED_DIR; ?>/editpost.php?action=makeDraft&id=<?php echo $post->getID(); ?>" type="button" class="button-dark">Make draft</a>
 							<!-- Dmytro asked for this -->
 							<?php 
-							if($user->getRoleID() > 1){
+							if($user->getRoleID() == 1){
 							?>
 							<a href="/<?php echo INSTALLED_DIR; ?>/afile.php" type="button" class="button-dark">Approve</a>
 							<?php
