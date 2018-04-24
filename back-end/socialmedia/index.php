7d9aa553ccfdf28b663c03f959f29a6bb8d1a869
Hannah Lias <HannahLias@Hannahs-MBP.lan>
2018-04-24 13:51:40 +0100

    Updated config file for social media tools

--- back-end/socialmedia/index.php
+++ back-end/socialmedia/index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
   <title>SHAID Social Media</title>
@@ -54,14 +53,14 @@
       <div class="row content">
         <div class="col-sm-6 text-left">
 
-        <a class="twitter-timeline" data-width="220" data-height="500" href="https://twitter.com/test19022018?ref_src=twsrc%5Etfw">Tweets by test19022018</a>
+        <a class="twitter-timeline" data-width="220" data-height="500" href="https://twitter.com/<?php echo $twitter_name; ?>?ref_src=twsrc%5Etfw">Tweets by <?php echo $twitter_name; ?></a>
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
         <div class="col-sm-6 text-left">
 
-        <div class="fb-page" data-href="https://www.facebook.com/SHAIDTest" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
-          <blockquote cite="https://www.facebook.com/SHAIDTest" class="fb-xfbml-parse-ignore">
-          <a href="https://www.facebook.com/SHAIDTest">Test2</a>
+        <div class="fb-page" data-href="https://www.facebook.com/<?php echo $page_name; ?>" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false">
+          <blockquote cite="https://www.facebook.com/<?php echo $page_name; ?>" class="fb-xfbml-parse-ignore">
+          <a href="https://www.facebook.com/<?php echo $page_name; ?>"><?php echo $page_name; ?></a>
           </blockquote>
         </div>
         </div>
@@ -106,7 +105,7 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=.<?php $app_id; ?>.&autoLogAppEvents=1';
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=<?php echo $app_id; ?>&autoLogAppEvents=1';
   fjs.parentNode.insertBefore(js, fjs);
   }
   (document, 'script', 'facebook-jssdk'));
--- back-end/socialmedia/post.php
+++ back-end/socialmedia/post.php
@@ -1,71 +1,71 @@
-    <?php
+<?php
 
-    require "twitteroauth/autoload.php";
-    use Abraham\TwitterOAuth\TwitterOAuth;
-    // create a new instance
-    $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
+require "twitteroauth/autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+// create a new instance
+$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
 
 
-    define('FACEBOOK_SDK_V4_SRC_DIR', __DIR__ . '/src/Facebook/');
-    require_once(__DIR__ . '/src/Facebook/autoload.php');
+define('FACEBOOK_SDK_V4_SRC_DIR', __DIR__ . '/src/Facebook/');
+require_once(__DIR__ . '/src/Facebook/autoload.php');
 
-    $fb = new Facebook\Facebook([
-        'app_id' => $app_id,
-        'app_secret' => $app_secret,
-        'default_graph_version' => 'v2.2',
-    ]);
+$fb = new Facebook\Facebook([
+    'app_id' => $app_id,
+    'app_secret' => $app_secret,
+    'default_graph_version' => 'v2.2',
+]);
 
-    //POST
-    if(isset($_POST['submit']))
-    {
-      $box = $_POST["test"];
-      if(!empty($box)){
-        $sm = implode('', $box);
-        if(strpos($sm, 'Facebook') !== false){
+//POST
+if(isset($_POST['submit']))
+{
+  $box = $_POST["test"];
+  if(!empty($box)){
+    $sm = implode('', $box);
+    if(strpos($sm, 'Facebook') !== false){
 
-          $text = htmlspecialchars($_POST['input']);
-          if(isset($accessToken)) {
-              $message = [
-                  'message' => "$text",
-              ];
+      $text = htmlspecialchars($_POST['input']);
+      if(isset($accessToken)) {
+          $message = [
+              'message' => "$text",
+          ];
 
-              try {
-                  $response = $fb->post('/'.$page_id.'/feed', $message, $accessToken);
-                  echo nl2br("\nPosted to Facebook \n");
-              } catch(Facebook\Exceptions\FacebookResponseException $e) {
-                  echo 'Graph returned an error: '.$e->getMessage();
-                  exit;
-              } catch(Facebook\Exceptions\FacebookSDKException $e) {
-                  echo 'Facebook SDK returned an error: '.$e->getMessage();
-                  exit;
-              }
-            }
-          }
-
-        if(strpos($sm, 'Twitter') !== false){
-          $text = htmlspecialchars($_POST['input']);
-          if(strlen($text)<=280){
-            $tweet->post('statuses/update', array('status' => "$text"));
-            echo nl2br("\nPosted to Twitter \n");
-          }
-          elseif (strpos($sm, 'Facebook') !== false){
-            echo("Message is longer than 280 characters, will be shortened and a link to the Facebook post will be provided");
-            $text = substr($text, 0,250);
-            $link = "https://fb.me/SHAIDTest";
-            $text = $text."...". $link;
-            $tweet->post('statuses/update', array('status' => "$text"));
-            echo nl2br("\nPosted to Twitter \n");
-
-          }
-
-          else {
-            echo("Message is ".(strlen($text)-280) ." characters too long, unable to post to Twitter");
+          try {
+              $response = $fb->post('/'.$page_id.'/feed', $message, $accessToken);
+              echo nl2br("\nPosted to Facebook \n");
+          } catch(Facebook\Exceptions\FacebookResponseException $e) {
+              echo 'Graph returned an error: '.$e->getMessage();
+              exit;
+          } catch(Facebook\Exceptions\FacebookSDKException $e) {
+              echo 'Facebook SDK returned an error: '.$e->getMessage();
+              exit;
           }
         }
-    }
-    }
+      }
 
-    //Future things to add: Add other social medias?
-    //                      Option to add pictures etc.
-    //                      Could add login buttons to make more transferrable
-    ?>
+    if(strpos($sm, 'Twitter') !== false){
+      $text = htmlspecialchars($_POST['input']);
+      if(strlen($text)<=280){
+        $tweet->post('statuses/update', array('status' => "$text"));
+        echo nl2br("\nPosted to Twitter \n");
+      }
+      elseif (strpos($sm, 'Facebook') !== false){
+        echo("Message is longer than 280 characters, will be shortened and a link to the Facebook post will be provided");
+        $text = substr($text, 0,250);
+        $link = "https://fb.me/$page_name";
+        $text = $text."...". $link;
+        $tweet->post('statuses/update', array('status' => "$text"));
+        echo nl2br("\nPosted to Twitter \n");
+
+      }
+
+      else {
+        echo("Message is ".(strlen($text)-280) ." characters too long, unable to post to Twitter");
+      }
+    }
+}
+}
+
+//Future things to add: Add other social medias?
+//                      Option to add pictures etc.
+//                      Could add login buttons to make more transferrable
+?>
