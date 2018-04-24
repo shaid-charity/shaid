a1e8c62e47baa594760ac2b9f3859f7a0eea4aa5
dmytro_drobisher <dimon161295@gmail.com>
2018-04-24 23:21:37 +0100

    social media

--- blog/admin/socialPost.php
+++ blog/admin/socialPost.php
@@ -46,5 +46,14 @@
     </div>
   </div>
   </body>
+  <script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.12&appId=<?php echo $app_id; ?>&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+  }
+  (document, 'script', 'facebook-jssdk'));
+</script>
 </html>
 
