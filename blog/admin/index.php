1eb63ddfb5fe0afc67f431800a82c4d2dc492751
Matthew Watson <matthew.accounts@gmx.com>
2018-04-26 18:38:08 +0100

    Began adding admin index

---
+++ blog/admin/index.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Index";
+define('CURRENT_PAGE', 'index');
+
+require_once '../includes/settings.php';
+require_once '../includes/config.php';
+require_once '../includes/db.php';
+require_once '../includes/functions.php';
+require_once 'header.php';
+
+?>
+
+<div class="container">
+	<div class="page-header"><h1>Admin Index</h1></div>
+	
