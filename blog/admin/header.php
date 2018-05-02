129b79565fe169107537a9a828944d0349483ed8
Caleb Hamilton <cjlhamilton@gmail.com>
2018-05-02 16:19:45 +0100

    Added index for admin panel

--- blog/admin/header.php
+++ blog/admin/header.php
@@ -131,7 +131,7 @@ if(empty($session_number)){
 <body>
   <!-- Top navbar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light mb-2">
-    <a class="navbar-brand" href="#">SHAID Admin Panel</a>
+    <a class="navbar-brand" href="index.php">SHAID Admin Panel</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- blog/admin/index.php
+++ blog/admin/index.php
@@ -5,11 +5,24 @@ define('CURRENT_PAGE', 'index');
 require_once '../includes/settings.php';
 require_once '../includes/config.php';
 require_once '../includes/db.php';
-require_once '../includes/functions.php';
 require_once 'header.php';
 
 ?>
 
-<div class="container">
-	<div class="page-header"><h1>Admin Index</h1></div>
-	
+  <div class="container">
+    <div class="page-header">
+      <h1>Admin Panel</h1>
+    </div>
+    <br/>
+
+    <p>Test</p>
+
+    <ul>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+    </ul>
+  </div>
+</body>
+</html>
